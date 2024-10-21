bdd2c5e2bd19c68f11e859776233297622d949c8
tahir ekrem koru <tahirekremkoru@gmail.com>
2024-10-22 01:03:18 +0300

    Finish ForEach

--- 07_ForeachLoop/Program.cs
+++ 07_ForeachLoop/Program.cs
@@ -19,13 +19,87 @@ namespace _07_ForeachLoop
             //3:In
             //4:Liste, Koleksiyon, Dizi
 
+            //1
+
+            //string[] cities = { "milano", "roma", "budapeşte", "ankara", "istanbul", "varşova" };
+
+            //foreach (string x in cities)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            //2
+
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            //foreach (int i in numbers)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //3
+
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            //foreach (int i in numbers)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //4
+
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            //int total = 0;
+            //foreach (int i in numbers)
+            //{
+            //    total += i;
+            //}
+            //Console.WriteLine(total);
+
+            //5
+
+            //List<int> numbers = new List<int>()
+            //{
+            //    1,2, 3, 4, 5, 6, 7,8
+            //};
+
+            //foreach (int i in numbers)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //6
+
+            //string word = "Merhaba";
+
+            //foreach (char c in word)
+            //{
+            //    Console.WriteLine(c);
+            //}
+
+
+
+            Console.Read();
+
+            #endregion
+
+            #region Örnek Sınav Sistemi Uygulaması
+
+
+
+
+
+
+
 
 
 
 
             #endregion
-
-
 
 
         }
