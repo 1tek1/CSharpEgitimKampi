e2c26cef8629a988ec99dd0d5b18c282bcf91308
tahir ekrem koru <tahirekremkoru@gmail.com>
2024-10-21 23:26:20 +0300

    Start foreach

---
+++ 07_ForeachLoop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ForeachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Foreach Döngüsü
+
+            //Foreach(1;2;3;4)
+
+            //1:Değişken
+            //2:Değişken Adı
+            //3:In
+            //4:Liste, Koleksiyon, Dizi
+
+
+
+
+
+            #endregion
+
+
+
+
+        }
+    }
+}
