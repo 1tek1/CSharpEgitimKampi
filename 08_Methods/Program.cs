cbdaf2b95323ae08dc027efb4685d595d96de465
tahir ekrem koru <tahirekremkoru@gmail.com>
2024-10-25 00:23:01 +0300

    Finish Methods

--- 08_Methods/Program.cs
+++ 08_Methods/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -95,15 +96,92 @@ namespace _08_Methods
 
             #region Geriye Değer Döndüren Metotlar
 
+            //1
 
+            //string CustomerName()
+            //{
+            //    return "Ali Yıldız";
+            //}
 
+            //Console.WriteLine(CustomerName());
 
+            //2
+
+            //string StudentCard()
+            //{
+            //    string name = "Ali";
+            //    string surname = "Yıldız";
+
+            //    return name + " " + surname;
+            //}
+
+            //Console.WriteLine(StudentCard());
+
+            #endregion
+
+            #region Geriye Değer Döndüren  Parametreli Metotlar
+
+            //string CountryCard(string countryName, string capital, string flagColor)
+            //{
+
+            //    string cardInfo = "Ülke: " + countryName + " - Başkent: " + capital + " - Bayrak Rengi: "
+            //        + flagColor;
+            //    return cardInfo;
+            //}
+
+            //string x, y, z;
+            //Console.Write("Ülke Adını Giriniz: ");
+            //x = Console.ReadLine();
+
+            //Console.Write("Başkenti Griniz: ");
+            //y = Console.ReadLine();
+
+            //Console.Write("Bayrak Rengini Giriniz: ");
+            //z = Console.ReadLine();
+
+            //Console.WriteLine(CountryCard(x, y, z));
+
+            //Console.WriteLine(CountryCard("Türkiye", "Ankara", "Kırmızı-Beyaz"));
+
+            #endregion
+
+            #region Geriye Değer Döndüren Int Parametreli Metotlar
+
+            //int Sum(int number1, int number2)
+            //{
+
+            //    int result = number1 + number2;
+            //    return result;
+            //}
+
+            //Console.WriteLine(Sum(1, 2));
+            //Console.WriteLine(Sum(3, 4));
 
 
 
 
             #endregion
-            Console.Read();
+
+            #region Örnek Uygulama
+
+            //string ExamResult(string student, int exam1, int exam2, int exam3)
+            //{
+            //    int result = (exam1 + exam2 + exam3) / 3;
+            //    if (result >= 50)
+            //    {
+            //        return student + " isimli öğrenci sınavı geçti" + " Ortalama: " + result;
+            //    }
+            //    else
+            //    {
+            //        return student + " isimli öğrenci dersten kaldı" + " Ortalama: " + result;
+            //    }
+            //}
+
+            //Console.WriteLine(ExamResult("Ali", 23, 45, 90));
+            //Console.WriteLine(ExamResult("Ayşe", 50, 50, 50));
+
+            #endregion
+            //Console.Read();
         }
     }
 }
