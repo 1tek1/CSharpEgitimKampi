91cf335f1d4955d090167831da470fec4ded524d
tahir ekrem koru <tahirekremkoru@gmail.com>
2024-10-28 21:35:06 +0300

    Sql part 1

--- 09_DatabaseProject/Program.cs
+++ 09_DatabaseProject/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +12,43 @@ namespace _09_DatabaseProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Veri Tabanı işlemleri");
+            //Ado.net
+            Console.WriteLine("***** C# Veri Tabanlı Ürün-Kategori Bilgi Sistemi *****");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string tableNumber;
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+            Console.Write("Lütfen getirmek istediğiniz tablo numarasını giriniz: ");
+            tableNumber = Console.ReadLine();
+            Console.WriteLine("-------------------------------------");
+
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-VT90LDB;initial " +
+                "Catalog=EgitimKampiDb;integrated security=true");
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("Select *From TblCategory", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            connection.Close();
+
+            foreach (DataRow row in dt.Rows)
+            {
+
+                foreach(var item in row.ItemArray)
+                {
+                    Console.Write(item.ToString());
+                }
+                Console.WriteLine();
+
+            }
             Console.Read();
         }
     }
