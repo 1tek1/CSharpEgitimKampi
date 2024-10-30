e85fbca64bd00e2221d5ad3d60f6a330d27f428d
tahir ekrem koru <tahirekremkoru@gmail.com>
2024-10-30 23:12:11 +0300

    Sql operations

--- 10_DatabaseCrud/Program.cs
+++ 10_DatabaseCrud/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,27 @@ namespace _10_DatabaseCrud
         static void Main(string[] args)
         {
             //Crud -> Create-Read-Update-Delete
+
+            Console.WriteLine("***** Menü Sipariş İşlem Paneli *****");
+            Console.WriteLine();
+
+            Console.WriteLine("--------------------------------");
+            Console.Write("Eklemek İstediğiniz Kategori Adı: ");
+            string categoryName = Console.ReadLine();
+
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-VT90LDB;initial catalog=EgitimKampiDb;integrated security=true");
+
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("insert into TblCategory (CategoryName) values (@p1)",connection);
+            command.Parameters.AddWithValue("@p1", categoryName);
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            Console.Write("Kategori başarıyla eklendi!");
+            Console.Read();
+
+
         }
     }
 }
